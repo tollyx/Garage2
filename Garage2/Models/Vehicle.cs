ef5704263afb2fbeba0a40dd015340dbef01e2f6
Adrian Hedqvist <adrian@tollyx.net>
2018-05-30 15:26:54 +0200

    Create Vehicle model

---
+++ Garage2/Models/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Models {
+    public class Vehicle {
+        public VehicleType Type { get; set; }
+        public string LicensePlate { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int WheelAmount { get; set; }
+        public DateTime CheckInTime { get; set; }
+    }
+
+    public enum VehicleType {
+        Car,
+        Boat,
+        Plane,
+        MotorCycle,
+    }
+}

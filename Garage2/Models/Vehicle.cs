5693fe6c5b5af0dc17033a374c37400d310153a3
Adrian Hedqvist <adrian@tollyx.net>
2018-06-04 11:07:13 +0200

    Add constraint to Vehicle.WheelsAmount

--- Garage2/Models/Vehicle.cs
+++ Garage2/Models/Vehicle.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Garage2.Models {
     public class Vehicle {
+        [Key]
         public int Id { get; set; }
         [DisplayName("Vehicle type")]
         public VehicleType Type { get; set; }
@@ -14,7 +16,9 @@ namespace Garage2.Models {
         public string Color { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
+
         [DisplayName("Number of wheels")]
+        [Range(0, 10)]
         public int WheelAmount { get; set; }
         public DateTime CheckInTime { get; set; }
     }

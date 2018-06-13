74b9f39ae648d2c28cdeaf1f31e32d2882e8dd57
Fredrik <blocket_konto@hotmail.se>
2018-06-13 14:46:13 +0200

    Change displayname change name in navbar

--- Garage2/Models/Member.cs
+++ Garage2/Models/Member.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace Garage2.Models {
     public class Member {
         public int Id { get; set; }
         public string Name { set; get; }
+        [DisplayName("Phone number")]
         public string PhoneNumber { set; get; }
+        [DisplayName("E-mail")]
         public string Email { set; get; }
+        [DisplayName("Registered since")]
         public DateTime RegisterDate { set; get; }
     }
 }
